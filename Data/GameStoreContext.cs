5ee65a99aa5e5f974988d34101d291016ec3d0be
Riden Shankhadev <fernandesriden1@gmail.com>
2024-09-18 20:37:37 +0545

    Initial commit

---
+++ Data/GameStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using GameStore.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data
+{
+    public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+    {
+        public DbSet<Game> Games => Set<Game>();
+        public DbSet<Genre> Genres => Set<Genre>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(
+                    new { Id = 1, Name = "Fighting"},
+                    new { Id = 2, Name = "Sports" },
+                    new { Id = 3, Name = "Action" },
+                    new { Id = 4, Name = "Adventure" },
+                    new { Id = 5, Name = "Survival" }
+                );
+        }
+    }
+}
